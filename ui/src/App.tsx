5758e76ce0c1b5eba6a3e2c3bec902839fed0e01
Alexandru Dochitoiu <alexdochitoiu@gmail.com>
2023-09-29 23:12:52 +0300

    Ui - cleanup & fetch events

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { apiUrl } from "./global";
+
+type Event = {
+  event_id: string;
+  event_name: string;
+  odds: number;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [events, setEvents] = useState<Event[]>([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/api/events`)
+      .then((res) => res.json())
+      .then((data) => setEvents(data))
+      .catch((error) => console.error("Error fetching events: " + error));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {events.map((event) => (
+        <div>
+          {event.event_name} - {event.odds}
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
