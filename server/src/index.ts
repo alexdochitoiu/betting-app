1ded84e8d2b906c9311cc80d38ceb320639c9def
Alexandru Dochitoiu <alexdochitoiu@gmail.com>
2023-09-29 23:11:47 +0300

    Server - add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,18 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import createTable from "./database/createTable";
 import seedDatabase from "./database/seedDatabase";
 import { getAllEvents } from "./controllers/event";
 
 const app = express();
-const port = process.env.PORT || 4000;
+app.use(cors());
 
 createTable().then(() => seedDatabase());
 
 app.get("/api/events", getAllEvents);
 
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`[Server]: API started on port ${port}`);
 });
